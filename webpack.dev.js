02405fe9fbc86fed865e7b95e30749d0de120d72
Boda <bodazhao@gmail.com>
2018-07-15 21:46:16 +0100

    added http-proxy-middleware
    to handle non-homepage page requests

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,5 +1,7 @@
 const path = require("path");
 const merge = require("webpack-merge");
+const convert = require("koa-connect");
+const proxy = require("http-proxy-middleware");
 const historyApiFallback = require("koa2-connect-history-api-fallback");
 
 const common = require("./webpack.common.js");
@@ -20,6 +22,25 @@ module.exports = merge(common, {
       // location say /about, the server will fail to GET anything from /about. We use
       // HTML5 History API to change the requested location to the index we specified
       app.use(historyApiFallback());
+      app.use(
+        convert(
+          // Although we are using HTML History API to redirect any sub-directory requests to index.html,
+          // the server is still requesting resources like JavaScript in relative paths,
+          // for example http://localhost:8080/users/main.js, therefore we need proxy to
+          // redirect all non-html sub-directory requests back to base path too
+          proxy(
+            // if pathname matches RegEx and is GET
+            (pathname, req) => pathname.match("/.*/") && req.method === "GET",
+            {
+              // options.target, required
+              target: "http://localhost:8080",
+              pathRewrite: {
+                "^/.*/": "/" // rewrite back to base path
+              }
+            }
+          )
+        )
+      );
     }
   },
   module: {
